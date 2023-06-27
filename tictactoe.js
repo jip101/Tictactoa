bde7e7e5066fb98cc4204dd3c245842baaab066f
Jeremy <jip101@gmail.com>
2023-06-26 23:40:14 -0400

    add popup form

--- tictactoe.js
+++ tictactoe.js
@@ -47,6 +47,7 @@ const gameBoard = (() => {
 
 
     let start = () => {
+        closeMenu()
         console.log('Game Started')
         players.splice(0, 2)
         let user1 = createPlayer(player1.value ? player1.value : 'X', '<svg height="100%" width="100%" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-46.08 -46.08 552.93 552.93"> <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55 c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55 c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505 c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55 l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719 c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"></path></svg>')
@@ -91,11 +92,12 @@ const gameBoard = (() => {
                 if (player.groups[row] === 3) {
                     document.querySelector('#display').innerHTML = `${player.name} wins!`
                     win(player)
+                    openMenu()
                     console.log(`${player.name} wins!`)
                 }
                 else if (totalMoves >= 9) {
                     document.querySelector('#display').innerHTML = `Tied Game`
-
+                    openMenu()
                 }
             })
         }
@@ -139,6 +141,18 @@ const gameBoard = (() => {
 
     let newGameBut = document.querySelector('#newGame')
     newGameBut.addEventListener('click', clearBoard)
+
+    let playerSelect = document.querySelector('#playerSelect').style
+
+    let openMenu = () => {
+        playerSelect.display = 'flex'
+    }
+
+    let closeMenu = () => {
+        playerSelect.display = 'none'
+    }
+
+    openMenu()
 
     return {createPlayer, start, spaces, players}
 })();
