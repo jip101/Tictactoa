86c92b6055b94296573d861591b0ff436a2b3f07
Jeremy <jip101@gmail.com>
2023-07-01 17:29:11 -0400

    finish 2 player mode

--- tictactoe.js
+++ tictactoe.js
@@ -46,7 +46,6 @@ const gameBoard = (() => {
 
     let start = () => {
         closeMenu()
-        console.log('Game Started')
         players.splice(0, 2)
         let user1 = createPlayer(player1.value ? player1.value : 'X', '<svg height="100%" width="100%" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-46.08 -46.08 552.93 552.93"> <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55 c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55 c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505 c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55 l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719 c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"></path></svg>')
         let user2 = createPlayer(player2.value ? player2.value : 'O', '<svg xmlns="http://www.w3.org/2000/svg" height="100%" width="100%" viewBox="0 0 24 24" stroke-width="3" stroke="black" fill="none"> <path d="M18 9a5 5 0 0 0 -5 -5h-2a5 5 0 0 0 -5 5v6a5 5 0 0 0 5 5h2a5 5 0 0 0 5 -5v-6" /> </svg>')
@@ -131,7 +130,6 @@ const gameBoard = (() => {
         spaces[6].groups = ['left', 'bot', 'fSlash']
         spaces[7].groups = ['vMid', 'bot']
         spaces[8].groups = ['right', 'bot', 'bSlash']
-        console.log('Board Cleared')
         start()
     }
     //console.log(spaces)
